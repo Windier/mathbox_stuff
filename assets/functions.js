7b9e4b2a5eba2aa1f0890e7789fff75ca0816356
José Carlos <acc.sws@gmail.com>
2016-05-10 11:39:44 -0300

    functions

---
+++ assets/functions.js
@@ -0,0 +1,15 @@
+var cos = function(x){
+  return Math.cos(x)
+}
+var sin = function(x){
+  return Math.sin(x)
+}
+var pow = function(x,y){
+  return Math.pow(x,y)
+};
+var exp = function(x){
+  return Math.exp(x)
+};
+var sqrt = function(x){
+  return Math.sqrt(x)
+};
